77a4fdcb5105e2216f24b12d65d935c0b4b68cf8
natali1503 <nfv1503@gmail.com>
2024-12-14 21:22:36 +0500

    change data initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Box, CssBaseline } from '@mui/material';
 import { HomePage } from './pages/HomePage';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { initializeTasks } from './init';
+import { useDispatch } from 'react-redux';
+import { TasksDispatch } from './stor/taskStore';
 function App() {
+  const dispatch = useDispatch<TasksDispatch>();
+  useEffect(() => {
+    // Инициализация данных при запуске приложения
+    dispatch(initializeTasks());
+  }, [dispatch]);
   return (
     <Box>
       <CssBaseline />
--- src/stor/taskStore.ts
+++ src/stor/taskStore.ts
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import taskReducer from './taskSlice';
 
-import { initializeTasks } from '../init';
-
 export const rootReducer = {
   tasks: taskReducer,
 };
@@ -10,6 +8,6 @@ export const rootReducer = {
 export const store = configureStore({
   reducer: rootReducer,
 });
-store.dispatch(initializeTasks());
+
 export type RootState = ReturnType<typeof store.getState>;
 export type TasksDispatch = typeof store.dispatch;
