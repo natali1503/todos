5f1edb0c53b882430e3ed9aeaa7ff9e767a26e3b
natali1503 <nfv1503@gmail.com>
2024-12-16 11:37:30 +0500

    add editing task

--- __test__/editingTask.test.js
+++ __test__/editingTask.test.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { waitFor } from '@testing-library/react';
+import { waitFor, fireEvent } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
 import App from '../src/App';
 import { screen, render } from './test-utils';
 
-describe('removeTask', () => {
-  it('Remove new task', async () => {
+describe('Editing task', () => {
+  afterEach(() => {
+    localStorage.clear();
+  });
+  it('Editing task', async () => {
     render(<App />, {});
     const taskInput = await waitFor(() => screen.getByTestId('taskInput'));
     await userEvent.type(taskInput, 'Test task');
     const addTaskBtn = screen.getByTestId('addTask');
     await userEvent.click(addTaskBtn);
     const taskItem = await waitFor(() => screen.getByDisplayValue('Test task'));
-    const taskContainer = taskItem.closest('div');
-    const removeTaskBtn = taskContainer.nextSibling;
-    await userEvent.click(removeTaskBtn);
+    await userEvent.type(taskItem, ' edit');
+    fireEvent.keyUp(taskItem, { key: 'Enter', code: 'Enter', charCode: 13 });
     await waitFor(() => {
-      expect(taskItem).not.toBeInTheDocument();
+      expect(taskItem).toHaveValue('Test task edit');
     });
   });
 });
